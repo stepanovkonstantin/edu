3180410d718dbb2c78556e863f0c2704518bd5a2
kstepanov <konstantinus.stepanovichus@gmail.com>
2021-09-08 20:00:47 +0300

    +input data

--- py_lessons_stage1/secretary.py
+++ py_lessons_stage1/secretary.py
@@ -2,6 +2,7 @@ documents = [
     {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
     {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
     {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+    {"type": "passport", "number": "4521 123456", "name": "Константин Степанов"}
 ]
 
 directories = {
